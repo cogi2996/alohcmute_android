7bd29236b2ab595a172abd60d0e47f0496f27e19
cogi2996 <bidang.tuyloan@gmail.com>
2024-05-16 23:46:38 +0700

    init

--- app/src/main/java/com/example/instagramapp/Login.java
+++ app/src/main/java/com/example/instagramapp/Login.java
@@ -12,6 +12,11 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.instagramapp.ModelAPI.AuthenticationRequest;
+import com.example.instagramapp.ModelAPI.AuthenticationResponse;
+import com.example.instagramapp.ModelAPI.LoginResponse;
+import com.example.instagramapp.retrofit.APIService;
+import com.example.instagramapp.retrofit.RetrofitClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
@@ -20,7 +25,12 @@ import com.google.firebase.auth.FirebaseAuth;
 
 import com.example.instagramapp.ReusableCode.ReusableCodeForAll;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class Login extends AppCompatActivity {
+    APIService apiService;
 
     TextView createacc;
     TextInputLayout Email, Pass;
@@ -32,20 +42,12 @@ public class Login extends AppCompatActivity {
     String password;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         createacc = (TextView) findViewById(R.id.signup);
-//        createacc.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(Login.this, Registration.class);
-//                startActivity(intent);
-//                finish();
-//            }
-//        });
 
         try {
             Email = (TextInputLayout) findViewById(R.id.login_email);
@@ -53,8 +55,6 @@ public class Login extends AppCompatActivity {
             login = (Button) findViewById(R.id.Login_btn);
             loginwithfacebook = (TextView) findViewById(R.id.login_facebook);
             Forgotpassword = (TextView) findViewById(R.id.forgotpass);
-
-
             FAuth = FirebaseAuth.getInstance();
 
             login.setOnClickListener(new View.OnClickListener() {
@@ -75,6 +75,8 @@ public class Login extends AppCompatActivity {
                             public void onComplete(@NonNull Task<AuthResult> task) {
 
                                 if (task.isSuccessful()) {
+
+
 
                                     mDialog.dismiss();
                                     if (FAuth.getCurrentUser().isEmailVerified()) {
@@ -101,16 +103,7 @@ public class Login extends AppCompatActivity {
                 }
             });
 
-//            txt.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//
-//                    Intent Register = new Intent(ChefLogin.this, ChefRegisteration.class);
-//                    startActivity(Register);
-//                    finish();
-//
-//                }
-//            });
+
         createacc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -121,15 +114,6 @@ public class Login extends AppCompatActivity {
         });
 
 
-//            Forgotpassword.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    Intent a = new Intent(Login.this, ForgotPassword.class);
-//                    startActivity(a);
-//                    finish();
-//
-//                }
-//            });
 
         } catch (Exception e) {
             Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
@@ -155,7 +139,6 @@ public class Login extends AppCompatActivity {
                 Email.setErrorEnabled(true);
                 Email.setError("Enter a valid Email Address");
             }
-
         }
         if (TextUtils.isEmpty(password)) {
             Pass.setErrorEnabled(true);
@@ -165,5 +148,30 @@ public class Login extends AppCompatActivity {
         }
         isvalid = (isvalidemail && isvalidpassword) ? true : false;
         return isvalid;
+    }
+    public Boolean signupInToServer(){
+        apiService = RetrofitClient.getRetrofit().create(APIService.class);
+        AuthenticationRequest authenticationRequest = AuthenticationRequest.builder()
+                .email(email)
+                .password(password)
+                .build();
+        // login
+        apiService.login(authenticationRequest).enqueue(new Callback<AuthenticationResponse>() {
+            @Override
+            public void onResponse(Call<AuthenticationResponse> call, Response<AuthenticationResponse> response) {
+                AuthenticationResponse authenticationResponse = response.body();
+                String access_token = authenticationResponse.getAccessToken();
+
+            }
+
+            @Override
+            public void onFailure(Call<AuthenticationResponse> call, Throwable throwable) {
+
+            }
+
+
+        });
+
+        return false;
     }
 }
